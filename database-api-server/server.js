88ae5572f83c2c7612be56a046f711ee42a0bc79
Paul Murray <plmrry@gmail.com>
2015-07-10 11:22:02 -0500

    server changes

--- database-api-server/server.js
+++ database-api-server/server.js
@@ -1,8 +1,30 @@
 var express = require("express");
+var bodyParser = require('body-parser')
+var MongoClient = require('mongodb').MongoClient
+var assert = require("assert");
 var app = express();
 
-app.get("/", function(request, response) {
-    response.end("well hi there.");
+var db_name = "index_cards_v2";
+var db_url = "mongodb://127.0.0.1:27017/" + db_name;
+
+var db = MongoClient.connect(db_url);
+
+app.get("/matches/score-above-zero.json", function(request, response) {
+    db.then(function(db) {
+        return db.collection("card_matches").find({ score: { $gt: 0 } }).toArray()
+    })
+    .then(function(json) {
+        response.json(json);
+    })
+});
+
+app.get("/matches/all-matches-count.json", function(request, response) {
+    db.then(function(db) {
+        return db.collection("card_matches").count()
+    })
+    .then(function(json) {
+        response.json(json);
+    })
 });
 
 var server = app.listen(8080, function () {
