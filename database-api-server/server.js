c2908ea4cfee422e1cd52a586dc1256a35d2a937
Paul Murray <plmrry@gmail.com>
2015-07-13 16:09:52 -0500

    add conflict API call

--- database-api-server/server.js
+++ database-api-server/server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var bodyParser = require('body-parser')
-var MongoClient = require('mongodb').MongoClient
+var mongodb = require('mongodb')
+var MongoClient = mongodb.MongoClient;
+var ObjectID = mongodb.ObjectID;
 var assert = require("assert");
 var app = express();
 
@@ -15,26 +17,37 @@ app.use(function(req, res, next) {
     next();
 });
 
+app.use(bodyParser.json({ strict: false }));
+
 var i = 0;
 
-app.get("/foo/:limit", function(request, response) {
-    response.end(request.params.limit);
+app.get("/all-with-conflict", function(request, response) {
+    db.then(function(db) {
+        return db.collection("card_matches")
+            .find({
+                score: { $gt: 0 },
+                "match.100": { $exists: false }
+            }).toArray()
+    })
+    .then(function(array) {
+        var a = array
+            .filter(function(m) { return m.match.some(function(d) { return d.potentialConflict && d.score }) })
+        response.json(a);
+    })
 })
 
-app.post("/get-one", bodyParser, function(request, response) {
-    response.end("ack");
-    // response.end(request.body)
-    // response.write("[")
-    // db.then(function(db) {
-    //     return db.collection("card_matches")
-    //         .find({ score: { $gt: 0 }, _participant_b_ids: request.params.query })
-    //         .stream({ transform: function(doc) { return JSON.stringify(doc); }});
-    // })
-    // .then(function(stream) {
-    //     stream.once("end", function() { response.write("]"); response.end() });
-    //     stream.on("data", function(d) { response.write(d); response.write(",") });
-    // })
-    // // response.end(request.params.query)
+app.post("/get-one", function(request, response) {
+
+    body = request.body;
+
+    db.then(function(db) {
+        return db.collection(body.collection)
+            .findOne({ _id: ObjectID(body._id) })
+            .then(function(json) {
+                response.json(json)
+            })
+    })
+    // response.json(request.body);
 });
 
 app.get("/matches/score-above-zero/participant-b/:query", function(request, response) {
