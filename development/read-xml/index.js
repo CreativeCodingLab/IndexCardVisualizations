45185d043e633967fe347e6c9c12b8d15882181b
Paul Murray <plmrry@gmail.com>
2016-01-21 13:36:31 -0600

    add read-xml

---
+++ development/read-xml/index.js
@@ -0,0 +1,21 @@
+var Rx = require('rx');
+var path = require('path');
+var fs = require('fs');
+var assert = require('assert');
+
+var stream = Rx.Observable;
+
+var dataPath = path.resolve(__dirname, '..', 'example-data')
+
+var cardPath = path.resolve(dataPath, 'PMC29063-UAZ-r1-1.json');
+var xmlPath = path.resolve(dataPath, 'PMC29063.nxml');
+
+var indexCard = require(cardPath)
+
+// console.log(indexCard)
+
+stream.fromNodeCallback(fs.readFile)(xmlPath)
+  .subscribe(function(buffer) {
+    console.log(buffer.toString())
+    console.log('index card meta:', indexCard.meta)
+  })
