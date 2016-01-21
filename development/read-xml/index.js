b54095523f8a1c770244af223004e8ef64d0a055
Paul Murray <plmrry@gmail.com>
2016-01-21 13:46:51 -0600

    modify index.js

--- development/read-xml/index.js
+++ development/read-xml/index.js
@@ -3,7 +3,7 @@ var path = require('path');
 var fs = require('fs');
 var assert = require('assert');
 
-var stream = Rx.Observable;
+// var stream = Rx.Observable;
 
 var dataPath = path.resolve(__dirname, '..', 'example-data')
 
@@ -12,10 +12,15 @@ var xmlPath = path.resolve(dataPath, 'PMC29063.nxml');
 
 var indexCard = require(cardPath)
 
+fs.readFile(xmlPath, function(err, buffer) {
+  console.log(buffer.toString())
+  console.log('index card meta:', indexCard.meta)
+})
+
 // console.log(indexCard)
 
-stream.fromNodeCallback(fs.readFile)(xmlPath)
-  .subscribe(function(buffer) {
-    console.log(buffer.toString())
-    console.log('index card meta:', indexCard.meta)
-  })
+// stream.fromNodeCallback(fs.readFile)(xmlPath)
+//   .subscribe(function(buffer) {
+//     console.log(buffer.toString())
+//     console.log('index card meta:', indexCard.meta)
+//   })
